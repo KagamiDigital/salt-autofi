f0d0421f27aa7d7e3b02c4314cd6602396ad0a4b
josephbouassaf <joseph.bouassaf@mail.mcgill.ca>
2026-01-13 09:52:20 -0500

    added hyperswap bot

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { interactiveMode } from "./interactive";
 import * as chorus_one from "./strategies/chorus-one/index";
 import * as somnia from "./strategies/somnia/index";
 import * as aave from "./strategies/aave/index";
+import * as hyperSwap from "./strategies/hyperliquid/index";
 import { parseEther } from "ethers/lib/utils";
 import { SaltAgent, Strategy } from "./agent/index";
 import { Salt } from "salt-sdk";
@@ -25,6 +26,12 @@ export const salt = new Salt({ environment: "TESTNET" });
   } else if (AGENT === "AAVE") {
     const strategy: Strategy = {
       sweepFunction: aave.deposit,
+    };
+    const agent = new SaltAgent(signer, strategy, parseEther("0.1"));
+    agent.init();
+  } else if (AGENT === "HYPERSWAP") {
+    const strategy: Strategy = {
+      sweepFunction: hyperSwap.swap,
     };
     const agent = new SaltAgent(signer, strategy, parseEther("0.1"));
     agent.init();
--- src/strategies/hyperliquid/lib/swap.ts
+++ src/strategies/hyperliquid/lib/swap.ts
@@ -1,10 +1,8 @@
-import { BigNumber, ethers } from "ethers";
-import { routerContract, TOKEN_IN, TOKEN_OUT, tokenContract } from "..";
-import { approve } from "./approve";
+import { BigNumber } from "ethers";
+import { routerContract, TOKEN_IN, TOKEN_OUT } from "..";
+
 import { sendTransaction, sendTransactionDirect } from "../../../salt";
 import { encodePath, FEE_TIERS } from "./path";
-import { broadcasting_network_provider } from "../../../config";
-import { formatEther, parseEther } from "ethers/lib/utils";
 
 const HYPE_CONSTANT = "0xadcb2f358eae6492f61a5f87eb8893d09391d160";
 
@@ -16,39 +14,12 @@ const HYPE_CONSTANT = "0xadcb2f358eae6492f61a5f87eb8893d09391d160";
 export async function swap({
   accountAddress,
   accountId,
+  amount,
 }: {
   accountAddress: string;
   accountId?: string;
+  amount?: BigNumber;
 }) {
-  let balance = undefined;
-  if (TOKEN_IN.toLowerCase() === HYPE_CONSTANT.toLowerCase()) {
-    balance = await broadcasting_network_provider.getBalance(TOKEN_IN);
-    balance = parseEther("0.001");
-  } else {
-    // Check tokenIn balance
-    balance = await tokenContract.balanceOf(accountAddress);
-    balance = BigNumber.from(balance);
-
-    console.log(`TokenIn Balance: ${balance.toString()}`);
-
-    // Check tokenIn allowance
-    const allowance = await tokenContract.allowance(
-      accountAddress,
-      routerContract.address
-    );
-    console.log(`TokenIn Allowance:`, allowance.toString());
-
-    if (allowance.lt(balance)) {
-      console.log(`Approving TokenIn for Router...`);
-
-      await approve({ accountId, accountAddress, amount: balance });
-
-      console.log(`TokenIn approved successfully.`);
-    } else {
-      console.log(`Sufficient TokenIn allowance already exists.`);
-    }
-  }
-
   const path =
     TOKEN_IN.toLowerCase() === HYPE_CONSTANT.toLowerCase()
       ? encodePath([TOKEN_IN, TOKEN_OUT], [FEE_TIERS.LOWEST])
@@ -65,7 +36,7 @@ export async function swap({
           fee: FEE_TIERS.LOWEST, // 100 = 0.01%
           recipient: accountAddress,
           deadline: Math.floor(Date.now() / 1000) + 60 * 20,
-          amountIn: balance,
+          amountIn: amount,
           amountOutMinimum: BigNumber.from(1),
           sqrtPriceLimitX96: BigNumber.from(0),
         }
@@ -73,7 +44,7 @@ export async function swap({
           path: path,
           recipient: accountAddress,
           deadline: Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from now
-          amountIn: balance, // Pass BigNumber directly
+          amountIn: amount, // Pass BigNumber directly
           amountOutMinimum: BigNumber.from(1), // Pass BigNumber directly
         };
 
@@ -89,7 +60,7 @@ export async function swap({
         recipient: routerContract.address,
         value:
           TOKEN_IN.toLowerCase() === HYPE_CONSTANT.toLowerCase()
-            ? balance
+            ? amount
             : BigNumber.from(0),
         data: data,
         accountId: accountId,
@@ -99,7 +70,7 @@ export async function swap({
         data: data,
         value:
           TOKEN_IN.toLowerCase() === HYPE_CONSTANT.toLowerCase()
-            ? balance
+            ? amount
             : BigNumber.from(0),
       });
 }
